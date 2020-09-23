8a8b23e7c92078765bbe71a279f1dac4e651b77a
WilliamBerlin76 <william.berlin76@gmail.com>
2020-09-23 16:35:58 -0500

    added products.json and product class

---
+++ Tutorial.WebSite/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tutorial.WebSite.Models
+{
+    public class Product
+    {
+        public string Id { get; set; }
+        public string Maker { get; set; }
+        public string Image { get; set; }
+        public string Url { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+    }
+}
