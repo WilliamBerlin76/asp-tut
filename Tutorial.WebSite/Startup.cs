218a3a9a992fadfb520d929efee832682be7a609
WilliamBerlin76 <william.berlin76@gmail.com>
2020-09-24 20:53:37 -0500

    added JsonFileProductService

--- Tutorial.WebSite/Startup.cs
+++ Tutorial.WebSite/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Tutorial.WebSite.Services;
 
 namespace Tutorial.WebSite
 {
@@ -24,6 +25,7 @@ namespace Tutorial.WebSite
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddTransient<JsonFileProductService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
