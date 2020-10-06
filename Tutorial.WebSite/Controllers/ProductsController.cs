2b284dbce575a88e0149c13582d513a3cf11a159
WilliamBerlin76 <william.berlin76@gmail.com>
2020-10-06 18:41:20 -0500

    created controller to get products

---
+++ Tutorial.WebSite/Controllers/ProductsController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Tutorial.WebSite.Models;
+using Tutorial.WebSite.Services;
+
+namespace Tutorial.WebSite.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        public ProductsController(JsonFileProductService productService)
+        {
+            this.ProductService = productService;
+        }
+
+        public JsonFileProductService ProductService { get; }
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return ProductService.GetProducts();
+        }
+    }
+}
